a303bac9fe05692997f088dd381526271b37c21b
Jason Mao <64656764+jasoncnm@users.noreply.github.com>
2023-02-04 16:51:52 -0500

    Main and puse menu
    Main menu and pause menu including resume game bottom, back to main menu bottom, rotate screen button, and quit bottom

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public string SceneName;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene(SceneName);
+    }
+}
---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool rotate = true;
+    public Camera Cm;
+    public static bool GameIsPause = false;
+    public GameObject PauseMenuUI;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPause)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPause = false;
+    }
+    void Pause()
+    {
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPause = true;
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 0f;
+        SceneManager.LoadScene("Start Menu");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+    
+    public void RotateCam()
+    {
+        if (rotate)
+        {
+            Cm.transform.Rotate(new Vector3(0, 0, 90));
+            rotate = false;
+        }
+        else
+        {
+            rotate = true;
+            Cm.transform.Rotate(new Vector3(0, 0, -90));
+        }
+    }
+}
