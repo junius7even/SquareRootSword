96f04785a6262e501b248cced8ec3ae0d88d9ba7
Haotian Mo <junius7even@gmail.com>
2023-02-04 14:09:08 -0500

    Create battle system
    - Player presses "h" and "L" to enter player turn (for testing purposes)
    - Player presses "a" to minus 1 health from enemy
    - Player hits return key to end his turn
    - Defeat condition is being a perfect square root

--- Assets/Scripts/BaseEnemy.cs
+++ Assets/Scripts/BaseEnemy.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class BaseEnemy : MonoBehaviour
 {
+    [field: SerializeField]
+    public Health health;
+
     // Start is called before the first frame update
     void Start()
     {
--- Assets/Scripts/BaseHero.cs
+++ Assets/Scripts/BaseHero.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class BaseHero : MonoBehaviour
 {
+    [field: SerializeField]
+    public Health health;
+
+    [field: SerializeField]
+    private BaseEnemy currentEnemy;
+
+    [field: SerializeField]
+    private Operator currentOperator;
+
+    public enum Operator{SquareRoot, Plus, Minus, Multiplication, Division,}
+
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void endTurn()
+    {
+   
     }
 }
