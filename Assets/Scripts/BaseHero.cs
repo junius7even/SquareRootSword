ccddba3c290c52470684c0e03cfd516b19851d2b
Allan Chew <awchew@uwaterloo.ca>
2023-02-04 15:55:31 -0500

    Add basic game logic to BaseCard

--- Assets/Scripts/BaseHero.cs
+++ Assets/Scripts/BaseHero.cs
@@ -4,6 +4,11 @@ using UnityEngine;
 
 public class BaseHero : MonoBehaviour
 {
+    [field: SerializeField]
+    public Operator currentOperator = Operator.Multiplication;
+
+    public enum Operator{SquareRoot, Plus, Minus, Multiplication, Division,};
+
     // Start is called before the first frame update
     void Start()
     {
