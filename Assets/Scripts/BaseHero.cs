17899e5ce7cd0a43165927191e93580d4117677d
Jason Mao <64656764+jasoncnm@users.noreply.github.com>
2023-02-03 23:18:59 -0500

    Test
    test

--- Assets/Scripts/BaseHero.cs
+++ Assets/Scripts/BaseHero.cs
@@ -7,7 +7,7 @@ public class BaseHero : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        int test = 0;
     }
 
     // Update is called once per frame
