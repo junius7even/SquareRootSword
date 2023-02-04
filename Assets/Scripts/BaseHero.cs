ac7eadffbb5aa7e6fc7b8f0e4440a77db87749cb
Allan Chew <awchew@uwaterloo.ca>
2023-02-04 17:02:55 -0500

    Fix conflict with BaseHero.cs

--- Assets/Scripts/BaseHero.cs
+++ Assets/Scripts/BaseHero.cs
@@ -4,22 +4,17 @@ using UnityEngine;
 
 public class BaseHero : MonoBehaviour
 {
+
     [field: SerializeField]
-<<<<<<< HEAD
     public Health health;
 
     [field: SerializeField]
     private BaseEnemy currentEnemy;
 
     [field: SerializeField]
-    private Operator currentOperator;
-
-    public enum Operator{SquareRoot, Plus, Minus, Multiplication, Division,}
-=======
     public Operator currentOperator = Operator.Multiplication;
 
     public enum Operator{SquareRoot, Plus, Minus, Multiplication, Division,};
->>>>>>> patch-2
 
     // Start is called before the first frame update
     void Start()
