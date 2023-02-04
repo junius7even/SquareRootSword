94ce10abe3ca4b8396a2ed694f7ff9591fa128d2
Allan Chew <awchew@uwaterloo.ca>
2023-02-04 12:58:01 -0500

    Make BaseCard draggable

--- Assets/Scripts/BaseCard.cs
+++ Assets/Scripts/BaseCard.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class BaseCard : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private bool dragging = false;
+    private Vector3 offset;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (dragging)
+        {
+            transform.position = GetMouseTransformPosition() + offset;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        offset = transform.position - GetMouseTransformPosition();
+        dragging = true;
+    }
+    private void OnMouseUp()
+    {
+        dragging = false;
+    }
+
+    private Vector3 GetMouseTransformPosition()
+    {
+        Vector3 screenPoint = Input.mousePosition;
+        screenPoint.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(screenPoint);
     }
 }
