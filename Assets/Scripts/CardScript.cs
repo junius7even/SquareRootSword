0bbab9d6c7d134c41eb1a3118b448908d1fe2083
Jackie Song <114444258+JackieSong99@users.noreply.github.com>
2023-02-04 22:32:50 -0500

    Card dragging.
    Able to drag the card into the candidate part.

---
+++ Assets/Scripts/CardScript.cs
@@ -0,0 +1,112 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    private bool dragging = false;
+    private Vector3 offset;
+    private Vector3 startPosition;
+    // Dictionary<BaseHero,int> overlappingHeros = new Dictionary<BaseHero, int>();
+    // private BaseHero closestHero;
+    public int cardValue;
+
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+    //     cardValue = Random.Range(1, 10); // currently initialize with random int in [1,9]
+    // }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dragging)
+        {
+            transform.position = GetMouseTransformPosition() + offset;
+        }
+    }
+
+    // try to use the card with an overlapping hero (return false if unable to use)
+    private bool TryUseCard()
+    {
+        // if (overlappingHeros.Count == 0) return false; // no hero to use with
+
+        // find closest hero
+        // float smallest_dist_so_far = float.PositiveInfinity;
+
+        // foreach (KeyValuePair<BaseHero, int> kvp in overlappingHeros)
+        // {
+        //     float cur_dist = Vector3.Distance(kvp.Key.transform.position, this.transform.position);
+        //     if (cur_dist < smallest_dist_so_far)
+        //     {
+        //         closestHero = kvp.Key;
+        //         smallest_dist_so_far = cur_dist;
+        //     }
+        // }
+
+        // TO DO: use cardValue
+        // BaseHero.Operator heroType = closestHero.currentOperator;
+        // if (heroType == BaseHero.Operator.Multiplication)
+        // {
+        //     Debug.Log("Multiplication " + cardValue);
+        // }
+        // else if (heroType == BaseHero.Operator.Division)
+        // {
+        //     Debug.Log("Division " + cardValue);
+        // }
+        // else if (heroType == BaseHero.Operator.Plus)
+        // {
+        //     Debug.Log("Plus " + cardValue);
+        // }
+        // else if (heroType == BaseHero.Operator.Minus)
+        // {
+        //     Debug.Log("Minus " + cardValue);
+        // }
+
+        // Destroy(gameObject); // destroy used card
+        return true;
+    }
+
+    private void OnMouseDown()
+    {
+        offset = transform.position - GetMouseTransformPosition();
+        startPosition = transform.position;
+        dragging = true;
+    }
+    private void OnMouseUp()
+    {
+        dragging = false;
+        if (!TryUseCard()) transform.position = startPosition; // if fail to use card, move it back to start position
+    }
+
+    private Vector3 GetMouseTransformPosition()
+    {
+        Vector3 screenPoint = Input.mousePosition;
+        screenPoint.z = Camera.main.nearClipPlane;
+        return Camera.main.ScreenToWorldPoint(screenPoint);
+    }
+
+    // public void EnterHeroNotify(GameObject colliderObject)
+    // {
+    //     BaseHero hero = colliderObject.GetComponent<BaseHero>();
+    //     if (hero == null) return;
+    //     overlappingHeros.Add(hero, 0);
+    // }
+
+    // public void ExitHeroNotify(GameObject colliderObject)
+    // {
+    //     BaseHero hero = colliderObject.GetComponent<BaseHero>();
+    //     if (hero == null) return;
+    //     overlappingHeros.Remove(hero);
+    // }
+
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Card selected successful." + collider.gameObject.name);
+    }
+
+    private void OnTriggerExit2D(Collider2D collider)
+    {
+        Debug.Log("Card discard TriggerExit2D: " + collider.gameObject.name);
+    }
+}
