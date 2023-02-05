0aeecc16cd3b0a987493aa1c050ad8784e75092a
Haotian Mo <junius7even@gmail.com>
2023-02-05 13:16:43 -0500

    Import assets and create health/attack prefabs

--- Assets/Scripts/DragIndicator.cs
+++ Assets/Scripts/DragIndicator.cs
@@ -11,6 +11,8 @@ public class DragIndicator : MonoBehaviour
     private Vector3 camOffset = new Vector3(0, 0, 10);
 
     private LineRenderer lr;
+    private Color c1 = Color.white;
+    private Color c2 = Color.white;
 
     [SerializeField] private AnimationCurve ac;
     // Start is called before the first frame update
@@ -27,6 +29,12 @@ public class DragIndicator : MonoBehaviour
             if (lr == null)
             {
                 lr = gameObject.AddComponent<LineRenderer>();
+                lr.material = new Material(Shader.Find("Sprites/Default"));
+                lr.startColor = c1;
+                lr.endColor = c2;
+                lr.startWidth = 1;
+                lr.endWidth = 5;
+                lr.sortingLayerName = "UI";
             }
             
             lr.enabled = true;
