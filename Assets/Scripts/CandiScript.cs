00247b89a6aadafc4eec52aba25114dd290c8c02
Zheng Zhu <operationzhu@163.com>
2023-02-05 12:52:12 -0500

    update dragging scripting and ui

--- Assets/Scripts/CandiScript.cs
+++ Assets/Scripts/CandiScript.cs
@@ -4,15 +4,11 @@ using UnityEngine;
 
 public class CandiScript : MonoBehaviour
 {
+    public CardScript isOccupiedBy;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        isOccupiedBy = null;
     }
 }
--- Assets/Scripts/CardScript.cs
+++ Assets/Scripts/CardScript.cs
@@ -7,15 +7,15 @@ public class CardScript : MonoBehaviour
     private bool dragging = false;
     private Vector3 offset;
     private Vector3 startPosition;
-    // Dictionary<BaseHero,int> overlappingHeros = new Dictionary<BaseHero, int>();
-    // private BaseHero closestHero;
     public int cardValue;
+    public CandiScript candiZone;
+
 
     // // Start is called before the first frame update
-    // void Start()
-    // {
-    //     cardValue = Random.Range(1, 10); // currently initialize with random int in [1,9]
-    // }
+    void Start()
+    {
+        startPosition = transform.position;
+    }
 
     // Update is called once per frame
     void Update()
@@ -27,56 +27,29 @@ public class CardScript : MonoBehaviour
     }
 
     // try to use the card with an overlapping hero (return false if unable to use)
-    private bool TryUseCard()
+    private bool TryEnterUpgradeZone()
     {
-        // if (overlappingHeros.Count == 0) return false; // no hero to use with
-
-        // find closest hero
-        // float smallest_dist_so_far = float.PositiveInfinity;
-
-        // foreach (KeyValuePair<BaseHero, int> kvp in overlappingHeros)
-        // {
-        //     float cur_dist = Vector3.Distance(kvp.Key.transform.position, this.transform.position);
-        //     if (cur_dist < smallest_dist_so_far)
-        //     {
-        //         closestHero = kvp.Key;
-        //         smallest_dist_so_far = cur_dist;
-        //     }
-        // }
-
-        // TO DO: use cardValue
-        // BaseHero.Operator heroType = closestHero.currentOperator;
-        // if (heroType == BaseHero.Operator.Multiplication)
-        // {
-        //     Debug.Log("Multiplication " + cardValue);
-        // }
-        // else if (heroType == BaseHero.Operator.Division)
-        // {
-        //     Debug.Log("Division " + cardValue);
-        // }
-        // else if (heroType == BaseHero.Operator.Plus)
-        // {
-        //     Debug.Log("Plus " + cardValue);
-        // }
-        // else if (heroType == BaseHero.Operator.Minus)
-        // {
-        //     Debug.Log("Minus " + cardValue);
-        // }
-
-        // Destroy(gameObject); // destroy used card
-        return true;
+        if (candiZone != null && candiZone.isOccupiedBy == this) {
+            transform.position = candiZone.transform.position;
+            return true;
+        }
+        return false;
     }
 
     private void OnMouseDown()
     {
         offset = transform.position - GetMouseTransformPosition();
-        startPosition = transform.position;
         dragging = true;
     }
     private void OnMouseUp()
     {
         dragging = false;
-        if (!TryUseCard()) transform.position = startPosition; // if fail to use card, move it back to start position
+        if (TryEnterUpgradeZone()) {
+            Debug.Log("SNAP TO GRID");
+        } else {
+            transform.position = startPosition; // if fail to use card, move it back to start position
+        }
+        
     }
 
     private Vector3 GetMouseTransformPosition()
@@ -86,27 +59,42 @@ public class CardScript : MonoBehaviour
         return Camera.main.ScreenToWorldPoint(screenPoint);
     }
 
-    // public void EnterHeroNotify(GameObject colliderObject)
-    // {
-    //     BaseHero hero = colliderObject.GetComponent<BaseHero>();
-    //     if (hero == null) return;
-    //     overlappingHeros.Add(hero, 0);
-    // }
+    public void EnterCandidateNotify(GameObject colliderObject)
+    {
+        CandiScript candidate = colliderObject.GetComponent<CandiScript>();
+        if (candidate == null) return;
 
-    // public void ExitHeroNotify(GameObject colliderObject)
-    // {
-    //     BaseHero hero = colliderObject.GetComponent<BaseHero>();
-    //     if (hero == null) return;
-    //     overlappingHeros.Remove(hero);
-    // }
+        if (candidate.isOccupiedBy == null) {
+            candidate.isOccupiedBy = this; 
+        }
+
+        Debug.Log("isoccupied." + candidate.isOccupiedBy);
+    }
+
+    public void ExitCandidateNotify(GameObject colliderObject)
+    {
+        CandiScript candidate = colliderObject.GetComponent<CandiScript>();
+        if (candidate == null) return;
+
+        if (candidate.isOccupiedBy == this) {
+            candidate.isOccupiedBy = null;
+        }
+
+        Debug.Log("isoccupied." + candidate.isOccupiedBy);
+    }
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
+    //     if (collider.is(Candidate)) 
+    //     Candidate hero = colliderObject.GetComponent<Candidate>();
+    // //     if (hero == null) return;
         Debug.Log("Card selected successful." + collider.gameObject.name);
+        EnterCandidateNotify(collider.gameObject);
     }
 
     private void OnTriggerExit2D(Collider2D collider)
     {
         Debug.Log("Card discard TriggerExit2D: " + collider.gameObject.name);
+        ExitCandidateNotify(collider.gameObject);
     }
 }
