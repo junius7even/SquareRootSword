b6dd928f04485b16810703ac5209b706bbadee31
Haotian Mo <junius7even@gmail.com>
2023-02-04 20:39:28 -0500

    Temp

--- Assets/Scripts/BaseHero.cs
+++ Assets/Scripts/BaseHero.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
+using TMPro;
 
 public enum Operator{SquareRoot, Plus, Minus, Multiplication, Division,};
 
@@ -13,23 +15,29 @@ public class BaseHero : MonoBehaviour
     [field: SerializeField]
     private BaseEnemy currentEnemy;
 
+
+    // UI elements
+    public TextMeshProUGUI operatorText;
+
     [field: SerializeField]
     public Operator currentOperator = Operator.Multiplication;
 
     // Start is called before the first frame update
     void Start()
     {
+        this.operatorText.text = Enum.GetName(typeof(Operator), currentOperator);
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        this.operatorText.text = Enum.GetName(typeof(Operator), currentOperator);
         
     }
 
     public void endTurn()
     {
-   
+        
     }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,17 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Health : MonoBehaviour
 {
     [field: SerializeField]
     public int maxHealth;
 
+    public TextMeshProUGUI healthText;
+
     public int currentHealth;
 
     // Start is called before the first frame update
     void Start()
     {
+        this.healthText.text = currentHealth.ToString();
         currentHealth = maxHealth;
+    }
+    void Update()
+    {
+        this.healthText.text = currentHealth.ToString();
     }
 }
